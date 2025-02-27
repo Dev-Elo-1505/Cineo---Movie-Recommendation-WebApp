2efdbfb71c8ec2f597147e311b3fa1c2d48f4b47
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-02-27 12:50:23 +0100

    feat: build navbar

--- app/ui/NavBar.tsx
+++ app/ui/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
+import { spaceGrotesk } from "./fonts";
 
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center p-5">
-      <h2>Cineo</h2>
+      <h2 className={`${spaceGrotesk.className} text-2xl`}>Cineo</h2>
       <div className="flex items-center space-x-2">
-          <IoSearchOutline />
-          <button>Filter</button>
+          <IoSearchOutline className="text-2xl" />
+          <button className="bg-[#ff6c37] py-2 px-3 rounded">Filter</button>
       </div>
     </nav>
   );
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, Space_Grotesk } from "next/font/google";
 
 export const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
+export const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
