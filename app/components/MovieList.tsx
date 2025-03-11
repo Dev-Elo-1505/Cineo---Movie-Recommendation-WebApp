dffe12c7cdc29d8af20faa209e788c4986de2ef3
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-03-11 15:48:50 +0100

    fix: fix horizontal scrolling issue

--- app/components/MovieList.tsx
+++ app/components/MovieList.tsx
@@ -16,12 +16,11 @@ interface Prop {
 const api_Url = "https://api.themoviedb.org/3/movie/";
 const api_Key = process.env.NEXT_PUBLIC_API_KEY;
 
-const MovieList = ({param}: Prop) => {
+const MovieList = ({ param }: Prop) => {
   const [movie, setMovie] = useState<Movie[]>([]);
-  const scrollRef = useRef(null)
-  const { scrollXProgress } = useScroll({ container: scrollRef });
-
-  const x = useTransform(scrollXProgress, [0, 1], ["0%", "-50%"]);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [scrollStart, setScrollStart] = useState(true);
+  const [scrollEnd, setScrollEnd] = useState(false);
 
   useEffect(() => {
     axios
@@ -33,12 +32,38 @@ const MovieList = ({param}: Prop) => {
         console.log(err);
       });
   }, [param]);
+
+   // Check Scroll Position
+   const checkScroll = () => {
+    if (scrollRef.current) {
+      setScrollStart(scrollRef.current.scrollLeft === 0);
+      setScrollEnd(
+        scrollRef.current.scrollLeft + scrollRef.current.clientWidth >=
+          scrollRef.current.scrollWidth
+      );
+    }
+  };
+
   return (
-    <div className="w-full overflow-hidden">
-       <div ref={scrollRef} className="relative w-full overflow-x-auto whitespace-nowrap scroll-smooth no-scrollbar">
-      <motion.div style={{ x }} className="flex gap-5 mx-auto w-max p-4">
+    <div className="relative w-full">
+
+{/* Indicators */}
+{!scrollStart && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-black/60 to-transparent h-full w-12 z-10 pointer-events-none"></div>
+      )}
+      {!scrollEnd && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-black/60 to-transparent h-full w-12 z-10 pointer-events-none"></div>
+      )}
+      <div ref={scrollRef}
+        onScroll={checkScroll}
+        className="overflow-x-auto whitespace-nowrap scroll-smooth no-scrollbar">
+           <motion.div
+        drag="x"
+        dragConstraints={{ left: -((movie.length - 1) * 200), right: 0 }}
+        className="flex gap-5 w-max p-4"
+      >
         {movie.map((movie) => (
-          <div key={movie.id} className="w-48">
+          <div key={movie.id} className="w-48 shrink-0">
             <div className="w-full rounded-lg shadow-lg">
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -46,14 +71,17 @@ const MovieList = ({param}: Prop) => {
                 className="w-full rounded-lg shadow-lg object-cover"
               />
             </div>
-            <h3 className="text-sm mt-2 font-semibold text-wrap">{movie.title}</h3>
+            <h3 className="text-sm mt-2 font-semibold text-wrap">
+              {movie.title}
+            </h3>
             <p className="text-xs text-gray-500">{movie.release_date}</p>
           </div>
         ))}
       </motion.div>
+        </div>
+
+     
     </div>
-    </div>
-   
   );
 };
 
