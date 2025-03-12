7a0f7f32f902206a563461bdd1d46ba276ecfd09
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-03-12 15:58:40 +0100

    update: move navbar component into layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Provider from "@/app/components/Provider";
 import { plusJakartaSans } from "@/app/components/fonts";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import NavBar from "./components/NavBar";
 
 export const metadata: Metadata = {
   title: "Cineo",
@@ -16,8 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${plusJakartaSans.className} antialiased`}>
-        <Provider>{children}</Provider>
+      <body className={`${plusJakartaSans.className} antialiased container mx-auto min-h-screen p-5 lg:px-20 lg:py-5`}>
+        <Provider>
+          <NavBar />
+          {children}
+        </Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,7 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div className="container mx-auto min-h-screen p-5 lg:px-20 lg:py-5">
-      <NavBar />
+    <div>
       <Movie />
     </div>
   );
